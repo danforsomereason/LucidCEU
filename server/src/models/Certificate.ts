f0396688c5d4e7dd59fc469d38d010e60bef0172
danforsomereason <147667936+danforsomereason@users.noreply.github.com>
2024-11-06 14:44:12 -0500

    Build models for Certificates and CourseProgress

---
+++ server/src/models/Certificate.ts
@@ -0,0 +1,55 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+interface IssueHistory {
+    issued_at: Date;
+    certificate_number: string;
+    quiz_score: number;
+}
+
+interface ICertificate extends Document {
+    user_id: mongoose.Schema.Types.ObjectId;
+    course_id: mongoose.Schema.Types.ObjectId;
+    // Include all instance so that you can later track annual compliance
+    issued_history: Array<{
+        issued_at: Date;
+        certificate_number: string;
+        quiz_score: number;
+    }>;
+    approved_by: Array<{
+        board: "NBCC" | "APA" | "ASWB" | "NAADAC" | "CAMFT" | "Nursing";
+        logo_url: string;
+    }>;
+
+    getMostRecentIssue(): IssueHistory | undefined;
+}
+
+const CertificateSchema: Schema = new Schema({
+    user_id: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    course_id: { type: Schema.Types.ObjectId, ref: "Course", required: true },
+    issued_history: [
+        {
+            issued_at: { type: Date, default: Date.now },
+            certificate_number: { type: String, required: true },
+            quiz_score: { type: Number, required: true },
+        },
+    ],
+    approved_by: [
+        {
+            board: {
+                type: String,
+                enum: ["NBCC", "APA", "ASWB", "NAADAC", "CAMFT", "Nursing"],
+                required: false,
+            },
+            logo_url: { type: String, required: false },
+        },
+    ],
+});
+
+CertificateSchema.methods.getMostRecentIssue = function () {
+    return this.issued_history.sort(
+        (a: IssueHistory, b: IssueHistory) =>
+            b.issued_at.getTime() - a.issued_at.getTime()
+    )[0];
+};
+
+export default mongoose.model<ICertificate>("Certificate", CertificateSchema);
---
+++ server/src/models/CourseProgress.ts
@@ -0,0 +1,19 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+interface ICourseProgress extends Document {
+    user_id: mongoose.Schema.Types.ObjectId;
+    course_id: mongoose.Schema.Types.ObjectId;
+    started_at: Date;
+    completed_at: Date;
+    status: 'in_progress' | 'completed' | 'not_started';
+}
+
+const CourseProgressSchema: Schema = new Schema({
+    user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    course_id: { type: Schema.Types.ObjectId, ref: 'Course', required: true },
+    started_at: { type: Date, default: Date.now },
+    completed_at: { type: Date },
+    status: { type: String, enum: ['in_progress', 'completed', 'not_started'], default: 'not_started' },
+});
+
+export default mongoose.model<ICourseProgress>('CourseProgress', CourseProgressSchema);
